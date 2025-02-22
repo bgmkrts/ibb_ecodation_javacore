6055e6a2e90761b14db0bd53b1420696f616fc7c
Begüm Karataş <begumkaratas18@gmail.com>
2025-02-22 16:49:01 +0300

    Student

--- src/main/java/com/begumkaratas/_3_week/Week3_07_Class_Examples_StudentInformationSystem.java
+++ src/main/java/com/begumkaratas/_3_week/Week3_07_Class_Examples_StudentInformationSystem.java
@@ -1,4 +1,9 @@
 package com.begumkaratas._3_week;
 
 public class Week3_07_Class_Examples_StudentInformationSystem {
+    public static void main(String[] args) {
+
+    }
+
+
 }
---
+++ src/main/java/com/begumkaratas/project_step1_file/Login.java
@@ -0,0 +1,4 @@
+package com.begumkaratas.project_step1_file;
+
+public class Login {
+}
